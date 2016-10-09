47ba5183bcb73fe7c43a09d0a0bbb2a881f4dabd
Luxepheray <elizabeth.mendoza.1@mypisd.net>
2016-10-09 14:02:07 -0500

    This better work this time and not be in Pizza

---
+++ Desktop/C++/Countdown/CountDown/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int i;
+    for (i=30;i>=0;i--){
+    std::cout<<i<<std::endl;
+    }
+
+}
--- Desktop/PIzza Assignment/PIzza Assignment/main.cpp
+++ Desktop/PIzza Assignment/PIzza Assignment/main.cpp
@@ -2,35 +2,34 @@
 
 using namespace std;
 
-int main()
-{
-std::cout<<"Welcome to This Weird Pizza Thing"<<std::endl;
+int main(){
+    std::cout<<"Welcome to This Weird Pizza Thing"<<std::endl;
 
-float i;
-std::cout<<"So, how many slices do you have per pizza pie?"<<std::endl;
-std::cin>>i;
+    int slicesPerPie;
+    std::cout<<"So, how many slices do you have per pizza pie?"<<std::endl;
+    std::cin>>slicesPerPie;
 
-float d;
-std::cout<<"How many pizza pies do you have?"<<std::endl;
-std::cin>>d;
+    float piesTotal;
+    std::cout<<"How many pizza pies do you have?"<<std::endl;
+    std::cin>>piesTotal;
 
-float a;
-std::cout<<"How much did it cost?"<<std::endl;
-std::cin>>a;
+    float costTotal;
+    std::cout<<"How much did it cost?"<<std::endl;
+    std::cin>>costTotal;
 
-float b;
-std::cout<<"How many people do you have?"<<std::endl;
-std::cin>>b;
+    int people;
+    std::cout<<"How many people do you have?"<<std::endl;
+    std::cin>>people;
 
-std:cout<<"So here's how many slices each person gets:"<<std::endl;
-std::cout<< (i*d)/b<<std::endl;
+    std::cout<<"So here's how many slices each person gets:"<<std::endl;
+    std::cout<< (slicesPerPie * piesTotal) / people <<std::endl;
 
-std::cout<<"And here's how much each person should pitch in:"<<std::endl;
-std::cout<< a/b<<std::endl;
+    std::cout<<"And here's how much each person should pitch in:"<<std::endl;
+    std::cout<< costTotal / people <<std::endl;
 
-std::cout<<"NOW GO AWAY"<<std::endl;
+    std::cout<<"NOW GO AWAY"<<std::endl;
 
-return 0;
+    return 0;
 }
 
 
