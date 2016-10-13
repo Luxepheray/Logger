7214816437e1466c1a645f15b3ce029b088cddc4
Luxepheray <elizabeth.mendoza.1@mypisd.net>
2016-10-12 21:38:14 -0500

    YAY IT BETTER WORK!

--- Desktop/C++/Countdown/CountDown/main.cpp
+++ Desktop/C++/Countdown/CountDown/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
-
+#include <time.h>
+#include <unistd.h>
 using namespace std;
 
 int main(){
-    int i;
-    std::cout<<"How many seconds?"<<std::endl;
-    for (std::cin>>i;i>=0;i--){
-    std::cout<<i<<std::endl;
+    int initTime;
+    std::cout << "How many seconds?" << std::endl;
+    for (std::cin >> initTime;initTime >= 0;initTime--){
+    std::cout << initTime<<std::endl;
+    sleep(1);
     }
+    return 0;
 
 }
