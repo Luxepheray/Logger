3822279541cdc319e09c87b167ddb90c41865b82
Luxepheray <elizabeth.mendoza.1@mypisd.net>
2016-10-17 19:53:21 -0500

    This better be in CardShuffler I swear

---
+++ Desktop/C++/CardShuffler/CardShuffler/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+using namespace std;
+
+class Numbers{
+    public:
+    int Ace;
+    int One;
+    int Two;
+    int Three;
+    int Four;
+    int Five;
+    int Six;
+    int Seven;
+    int Eight;
+    int Nine;
+    int Ten;
+    int Jack;
+    int Queen;
+    int King;
+   };
+class Suit{
+    public:
+    int Spades;
+    int Hearts;
+    int Diamonds;
+    int Clubs;
+};
+void loop(){
+    while(1>0){
+    Numbers i;
+    Suit d;
+    std::cout<< i rand(); <<std::endl;
+    std::cout << "of";
+    std::cout << d rand(); << std::endl;
+}
+}
