555e0169e60d0883b39ad9a5cb757fba0f9a7fb4
Luxepheray <elizabeth.mendoza.1@mypisd.net>
2016-10-17 21:50:25 -0500

    This should work

--- Desktop/C++/CardShuffler/CardShuffler/main.cpp
+++ Desktop/C++/CardShuffler/CardShuffler/main.cpp
@@ -1,38 +1,33 @@
 #include <iostream>
 #include <math.h>
 #include <cstdlib>
+#include <string>
+#include <unistd.h>
 using namespace std;
-
-class Numbers{
+class Cards{
     public:
-    int Ace;
-    int One;
-    int Two;
-    int Three;
-    int Four;
-    int Five;
-    int Six;
-    int Seven;
-    int Eight;
-    int Nine;
-    int Ten;
-    int Jack;
-    int Queen;
-    int King;
-   };
-class Suit{
-    public:
-    int Spades;
-    int Hearts;
-    int Diamonds;
-    int Clubs;
+    std::string Number [14] = {"Ace", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+    std::string Suits [4] = {"Spades", "Hearts", "Diamonds", "Clubs"};
+    std::string DANGCOLOR [2] = {"Red", "Black"};
 };
 void loop(){
-    while(1>0){
-    Numbers i;
-    Suit d;
-    std::cout<< i rand(); <<std::endl;
-    std::cout << "of";
-    std::cout << d rand(); << std::endl;
+    int i;
+    for(i=0;i<52;i++){
+    Cards cardshuffle;
+    std::cout << cardshuffle.DANGCOLOR[rand() %2] << std::flush;
+    std::cout << cardshuffle.Number[rand() %14] << std::flush;
+    std::cout << " of " << std::flush;
+    std::cout << cardshuffle.Suits [rand() %4] << std::endl;
+    sleep(1);
+    }
+
+   }
+int main(){
+srand(time(NULL));
+    loop();
+    return 0;
 }
-}
+
+
+
+
